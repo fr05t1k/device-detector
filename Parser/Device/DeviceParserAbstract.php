d6cca23a151bb91ddf0f5a79cad05e182b931665
sgiehl <stefan@matomo.org>
2019-07-22 17:35:49 +0200

    use declare(strict_types=1)

--- Parser/Device/DeviceParserAbstract.php
+++ Parser/Device/DeviceParserAbstract.php
@@ -1,4 +1,5 @@
-<?php
+<?php declare(strict_types=1);
+
 /**
  * Device Detector - The Universal Device Detection library for parsing User Agents
  *
@@ -735,7 +736,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
                 return true;
             }
 
-            $this->model = trim($this->buildModel($modelRegex['model'], $modelMatches));
+            $this->model = $this->buildModel($modelRegex['model'], $modelMatches);
 
             if (isset($modelRegex['brand']) && $brandId = array_search($modelRegex['brand'], self::$deviceBrands)) {
                 $this->brand = $brandId;
@@ -761,7 +762,7 @@ abstract class DeviceParserAbstract extends ParserAbstract
             return null;
         }
 
-        return $model;
+        return trim($model);
     }
 
     protected function reset()
